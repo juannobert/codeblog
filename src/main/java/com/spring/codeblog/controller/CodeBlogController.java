c27fdceeec1c23f72aa8bac47d0b0f46ed520973
Juan Nobert <nobert407@gmail.com>
2022-04-28 15:28:28 -0300

    Detalhes de um post especifico criado

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.codeblog.model.Post;
@@ -14,12 +15,24 @@ import com.spring.codeblog.service.CodeBlogService;
 public class CodeBlogController {
 	@Autowired
 	CodeBlogService service;
-	
-	@GetMapping("/")
+
+	@GetMapping("/posts")
 	public ModelAndView getPosts() {
-		ModelAndView mv =  new ModelAndView("posts");
+		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = service.findAll();
-		mv.addObject("posts",posts);
+		mv.addObject("posts", posts);
+		return mv;
+	}
+
+	@GetMapping("/posts/{id}")
+	public Object getPostById(@PathVariable long id) {
+		ModelAndView mv = new ModelAndView();
+		if (!service.findById(id).isPresent()) {
+			return "redirect:/posts";
+		}
+		Post post = service.findById(id).get();
+		mv.setViewName("postDetails");
+		mv.addObject("post", post);
 		return mv;
 	}
 }
