4b7d703679352cf9726d3d9eac520e9f4e5ce19d
Juan Nobert <nobert407@gmail.com>
2022-05-13 15:03:04 -0300

    Spring Security adicionado

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -2,8 +2,12 @@ package com.spring.codeblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.spring.codeblog.configuration.SecurityConfig;
 
 @SpringBootApplication
+@Import({SecurityConfig.class})
 public class CodeblogApplication {
 
 	public static void main(String[] args) {
