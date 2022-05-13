b4a7f29da7f0899be928d243e056ff17176d2428
Juan Nobert <nobert407@gmail.com>
2022-05-13 14:41:46 -0300

    Summernote e mensagens de validações adicionadas ao formulario

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -9,11 +9,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.dto.PostDto;
 import com.spring.codeblog.model.Post;
@@ -49,9 +49,11 @@ public class CodeBlogController {
 	}
 	
 	@PostMapping("newpost")
-	public String savePost(@Valid PostDto postDto,BindingResult result) {
-		if(result.hasErrors()) 
+	public String savePost(@Valid PostDto postDto,BindingResult result,RedirectAttributes atributes) {
+		if(result.hasErrors()) {
+			atributes.addFlashAttribute("mensagem","Preencha todos os campos");
 			return "redirect:/newpost";
+		}
 		Post post = new Post();
 		BeanUtils.copyProperties(postDto, post);
 		post.setData(LocalDate.now());
@@ -66,6 +68,4 @@ public class CodeBlogController {
 			service.delete(post);
 			return "redirect:/posts";
 	}
-	
-	
 }
